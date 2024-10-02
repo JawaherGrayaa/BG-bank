3ee393350591a658ced78f5087e36dbb361e387a
Jawaher Grayaa <grayaageweher@gmail.com>
2024-10-02 22:18:22 +0200

    BG-Bank

---
+++ home.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    
+   
+   
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  
+</head>
+
+<body>
+<div class="header">
+<?php
+$active="home";
+include('head.php'); ?>
+
+</div>
+<?php include'ticker.php'; ?>
+
+  <div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;   ">
+    <div class="container">
+    <div id="content-wrap"style="padding-bottom:75px;">
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+    
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+    </ul>
+
+   
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="image\R.jpg" alt="image\R.jpg" width="100%" height="500">
+      </div>
+      <div class="carousel-item">
+        <img src="image\blood_donation.jpg" alt="image\blood_donation.jpg" width="100%" height="500">
+      </div>
+
+    </div>
+
+   
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  </div>
+<br>
+<h1 style="text-align:center;font-size:40px;font-family:Georgia, sans-serif;"> 
+     <font color="616a6b"><br>Welcome to Blood Givers Bank :</br>A Blood Bank & Donor Management System</font>
+</h1>
+
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header  text-white" style="background-color:#5b2c6f;" >The need for blood</h4>
+
+                        <p class="card-body overflow-auto" style="padding-left:2%;height:120px;text-align:left;">
+                          <?php
+                            include 'conn.php';
+                            $sql=$sql= "select * from pages where page_type='needforblood'";
+                            $result=mysqli_query($conn,$sql);
+                            if(mysqli_num_rows($result)>0)   {
+                                while($row = mysqli_fetch_assoc($result)) {
+                                  echo $row['page_data'];
+                                }
+                              }
+
+                           ?>
+                         </p>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header  text-white" style="background-color:#5b2c6f;">Blood Tips</h4>
+
+                    <p class="card-body overflow-auto" style="padding-left:2%;height:120px;text-align:left;">
+                      <?php
+                        include 'conn.php';
+                        $sql=$sql= "select * from pages where page_type='bloodtips'";
+                        $result=mysqli_query($conn,$sql);
+                        if(mysqli_num_rows($result)>0)   {
+                            while($row = mysqli_fetch_assoc($result)) {
+                              echo $row['page_data'];
+                            }
+                          }
+
+                       ?>
+                     </p>
+
+                        </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header  text-white " style="background-color:#5b2c6f;" >Who you could Help</h4>
+
+                    <p class="card-body overflow-auto" style="padding-left:2%;height:120px;text-align:left;">
+                      <?php
+                        include 'conn.php';
+                        $sql=$sql= "select * from pages where page_type='whoyouhelp'";
+                        $result=mysqli_query($conn,$sql);
+                        if(mysqli_num_rows($result)>0)   {
+                            while($row = mysqli_fetch_assoc($result)) {
+                              echo $row['page_data'];
+                            }
+                          }
+
+                       ?>
+                     </p>
+
+
+                        </div>
+            </div>
+</div>
+
+        <h2>Blood Donor Names</h2>
+
+        <div class="row">
+          <?php
+            include 'conn.php';
+            $sql= "select * from donor_details join blood where donor_details.donor_blood=blood.blood_id order by rand() limit 6";
+            $result=mysqli_query($conn,$sql);
+            if(mysqli_num_rows($result)>0)
+            {
+            while($row = mysqli_fetch_assoc($result)) {
+           ?>
+            <div class="col-lg-4 col-sm-6 portfolio-item" ><br>
+            <div class="card" style="width:300px">
+                <img class="card-img-top" src="image\Donor Darah.png" alt="Card image" style="width:100%;height:300px">
+                <div class="card-body">
+                  <h3 class="card-title"><?php echo $row['donor_name']; ?></h3>
+                  <p class="card-text">
+                    <b>Blood Group : </b> <b><?php echo $row['blood_group']; ?></b><br>
+                    <b>Mobile No. : </b> <?php echo $row['donor_number']; ?><br>
+                    <b>Gender : </b><?php echo $row['donor_gender']; ?><br>
+                    <b>Age : </b> <?php echo $row['donor_age']; ?><br>
+                    <b>Address : </b> <?php echo $row['donor_address']; ?><br>
+                  </p>
+
+                </div>
+              </div>
+        </div>
+      <?php }} ?>
+</div>
+<br>
+       
+
+
+        <div class="row">
+            <div class="col-lg-6">
+                <h2 style="color: #5b2c6f; font-family: georgia;">BLOOD GROUPS</h2>
+                <p>
+                
+                    <img src="image\hhh.jpg" alt="image\hhh.jpg" width="100%" height="500">
+
+                   
+
+            </div>
+            <div class="col-lg-6">
+                <img class="img-fluid rounded" src="image/pp2.jpg" alt="image ici" >
+            </div>
+        </div>
+       
+
+        <hr>
+
+       
+        <div class="row mb-4">
+            <div class="col-md-8">
+            <h4 style="color: #5b2c6f; font-family: georgia;" >UNIVERSAL DONORS AND RECIPIENTS</h4>
+            <p>
+              <?php
+                include 'conn.php';
+                $sql=$sql= "select * from pages where page_type='universal'";
+                $result=mysqli_query($conn,$sql);
+                if(mysqli_num_rows($result)>0)   {
+                    while($row = mysqli_fetch_assoc($result)) {
+                      echo $row['page_data'];
+                    }
+                  }
+
+               ?></p>
+              </div>
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-secondary btn-block" href="donate_blood.php" style="align:center; background-color:#5b2c6f;color:#fff">Become a Donor </a>
+            </div>
+        </div>
+
+    </div>
+  </div>
+  <?php include('footer.php');?>
+</div>
+
+</body>
+
+</html>
